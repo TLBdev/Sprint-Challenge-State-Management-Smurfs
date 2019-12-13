20e5efd827fb96edc00d1dbeb17a94c2fe65e09d
tony <serratedhumor@yahoo.com>
2019-12-13 11:50:52 -0600

    stretch goal complete, total project time 01h:49m

--- smurfs/src/components/SmurfForms.js
+++ smurfs/src/components/SmurfForms.js
@@ -17,9 +17,39 @@ function SmurfForms() {
             .catch((err) => console.log(err))
         changeState({
             ...state,
+            name: '',
+            age: 0,
+            height: '',
+            id: 0,
             edited: state.edited + 1
         })
+    }
+    const handleAlter = (e) => {
+        e.preventDefault()
+        axios.put(`http://localhost:3333/smurfs/${state.id}`, { name: state.name, age: state.age, height: state.height, id: Date.now() })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err))
+        changeState({
+            ...state,
+            name: '',
+            age: 0,
+            height: '',
+            id: 0,
+            edited: state.edited + 1
+        })
+    }
+    const handleRemove = (e) => {
+        e.preventDefault()
+        axios.delete(`http://localhost:3333/smurfs/${state.id}`)
 
+        changeState({
+            ...state,
+            name: '',
+            age: 0,
+            height: '',
+            id: 0,
+            edited: state.edited + 1
+        })
     }
     return (
         <div>
@@ -29,11 +59,16 @@ function SmurfForms() {
                 <input name='height' value={state.height} onChange={handleChange} type='text'></input>
                 <button type='submit'>Add To Village</button>
             </form>
-            <form name='alter'>
-
+            <form name='alter' onSubmit={handleAlter}>
+                <input name='name' value={state.name} onChange={handleChange} type='text'></input>
+                <input name='age' value={state.age} onChange={handleChange} type='text'></input>
+                <input name='height' value={state.height} onChange={handleChange} type='text'></input>
+                <input name='id' value={state.id} onChange={handleChange} type='text'></input>
+                <button type='submit'>Alter Smurf by ID</button>
             </form>
-            <form name='delete'>
-
+            <form name='delete' onSubmit={handleRemove}>
+                <input name='id' value={state.id} onChange={handleChange} type='text'></input>
+                <button type='submit'>KIA by ID</button>
             </form>
         </div>
     )
